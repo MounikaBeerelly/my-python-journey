6bdf305af34c68df14521c4e21b0bf910b441f6f
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2025-10-11 14:56:58 -0500

    zip function examples

---
+++ 03-Loops/06-zip function/ex04.py
@@ -0,0 +1,19 @@
+#!python3
+
+import os
+os.system("cls")
+
+print(" ")
+print("\nIllustration of \"Zip\" function in python")
+print("--------------------OoO-------------------")
+
+itemsinBasket = ["Scissors", "Paper Packs", "Glue Sticks", "Erasers", "Staple Pins"]
+itemsQuantity = [10,25, 15, 20, 5]
+
+#finalBasket = zip(itemsinBasket, itemsQuantity) #Here the address to the location of the zipped object is returned
+#print("\nThe final basket is:", list(finalBasket), end="\n")
+
+print("The final basket with Items and Quantity..", end="\n")
+
+for basketItem, itemQuantity in zip(itemsinBasket,itemsQuantity):
+    print(f"Item {basketItem} is Purchased with a quantity of : {itemQuantity}", end="\n")
---
+++ 03-Loops/06-zip function/ex06.py
@@ -0,0 +1,17 @@
+#!python3
+
+import os
+os.system("cls")
+
+print(" ")
+print("\nIllustration of \"Zip\" function in python")
+print("--------------------OoO-------------------")
+
+itemsinBasket = ["Scissors", "Paper Packs", "Glue Sticks", "Erasers", "Staple Pins"]
+itemsPrice = [15.25, 125, 5.35, 3.25, 7.75]
+itemsQuantity = [10,25, 15, 20, 5]
+
+print("The final basket with Items , unit price and Quantity..", end="\n")
+
+for basketItem, itemPrice, itemQuantity in zip(itemsinBasket,itemsPrice,itemsQuantity):
+    print("Item %s is priced at %0.2f was Purchased with a quantity of %d, totlaing to %0.2f"%(basketItem,itemPrice,itemQuantity, (itemPrice*itemQuantity)), end="\n")
