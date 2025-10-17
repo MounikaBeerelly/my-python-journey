ed558148079ee6635d047d012bef333abfbf2883
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2025-10-17 17:32:41 -0500

    Added arange function example

---
+++ NumPy/03_Arange Function/Arange Function.md
@@ -0,0 +1,17 @@
+## Arange Function:
+
+### np.arange() function:
+
+1. The `np.arange()` function in NumPy generates evenly spaced values within a specified interval, creating a 1D array.
+
+### Basic Syntax:
+    `numpy.arange([start, ] stop,[step, ], stype=none)`
+
+- **start**: (Optional) Starting a value of the sequence. Default is 0
+- **stop**: (required) End value of the sequence (exclusive)
+- **step**: (optional) Difference between consecutive values. Default is 1
+- **dtype**: (optional) Specifies the desired data type of the array. If not provided, it is inferred
+
+### Note:
+- supports integer, float, or custom data types
+- When we want to generate the array with higher value to low value we have to definitely supply the step value in negative. Else an empty array generated.
--- NumPy/03_Arange Function/arange-function.py
+++ NumPy/03_Arange Function/arange-function.py
@@ -41,22 +41,3 @@ print(myArray08)
 myArray09 = np.arange(10,1)
 print(f"\nPrinting the elements from the array09...",end="\n")
 print(myArray09)
-
-"""
-1. The np.arange() function in NumPy generates evenly spaced values within a specified interval, creating a 1D array.
-
-Basic Syntax:
-------------
-numpy.arange([start, ] stop,[step, ], stype=none)
-
-1. start: (Optional) Starting a value of the sequence. Default is 0
-2. stop: (required) End value of the sequence (exclusive)
-3. step: (optional) Difference between consecutive values. Default is 1
-4. dtype: (optional) Specifies the desired data type of the array. If not provided, it is inferred
-
-Points to note:
---------------
-- supports integer, float, or custom data types
-
-- When we want to generate the array with higher value to low value we have to definitely supply the step value in negative. Else an empty array generated.
-"""
